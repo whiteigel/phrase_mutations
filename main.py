44ba6ce22c3da4f0664d76e11627de85979d7d15
Sergei <sergei.volodin1@ibm.com>
2023-12-04 12:38:08 +0100

    Minor changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def mutate_phrase(phrase, new_positions):
     words = phrase.split()
     original_positions = {word: words.index(word) for word in words}
 
-    # Mutate the seed phrase based on the new positions
+    # Mutate the phrase based on the new positions
     mutated_phrase = ' '.join(words[new_pos - 1] for new_pos in new_positions)
 
     return mutated_phrase, original_positions
@@ -13,12 +13,12 @@ def mutate_phrase(phrase, new_positions):
 
 def revert_phrase(mutated_phrase, original_positions):
     words = mutated_phrase.split()
-    # Revert the mutated seed phrase to the original
+    # Revert the mutated phrase to the original
     reverted_phrase = ' '.join(word for word, pos in sorted(original_positions.items(), key=lambda x: x[1]))
 
     return reverted_phrase
 
-# Mutate the seed phrase
+# Mutate the phrase
 mutated_phrase, original_positions = (mutate_phrase(original_phrase, new_positions))
 print("Mutated Phrase:", mutated_phrase)
 
